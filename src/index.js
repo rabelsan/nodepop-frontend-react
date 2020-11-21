cad6290f067c1a81f958aa0840ba46c25c89d3d4
rabelsan <rabelsan@gmail.com>
2020-11-21 08:16:17 +0100

    struct4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from '../components/App';
+import App from './components/App';
 import storage from './utils/storage';
 import { configureClient } from './api/client';
 
